6295a9e8187efc35cdf81aaa41693f89c8c561d1
Jab125 <67534807+Jab125@users.noreply.github.com>
2023-07-26 23:02:09 +1000

    reorder branding lines

--- minecraft/src/main/java/com/jab125/loader/LoaderBranding.java
+++ minecraft/src/main/java/com/jab125/loader/LoaderBranding.java
@@ -3,7 +3,7 @@ package com.jab125.loader;
 import net.minecraft.SharedConstants;
 
 public class LoaderBranding {
-    public static final String[] LINES = {"Tweak Loader " + getTweakLoaderVersion(), "Example Modloader 1.0-SNAPSHOT", "Minecraft " + SharedConstants.getGameVersion().getName()};
+    public static final String[] LINES = {"Example Modloader 1.0-SNAPSHOT", "Tweak Loader " + getTweakLoaderVersion(), "Minecraft " + SharedConstants.getGameVersion().getName()};
 
     private static String getTweakLoaderVersion() {
         return null;
