0d082ee7a0a83a3a0df6894470842619afe2fe57
Jab125 <67534807+Jab125@users.noreply.github.com>
2023-07-26 22:46:03 +1000

    add title screen branding

---
+++ minecraft/src/main/java/com/jab125/loader/LoaderBranding.java
@@ -0,0 +1,11 @@
+package com.jab125.loader;
+
+import net.minecraft.SharedConstants;
+
+public class LoaderBranding {
+    public static final String[] LINES = {"Tweak Loader " + getTweakLoaderVersion(), "Example Modloader 1.0-SNAPSHOT", "Minecraft " + SharedConstants.getGameVersion().getName()};
+
+    private static String getTweakLoaderVersion() {
+        return null;
+    }
+}
